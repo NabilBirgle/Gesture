b03f1afb12e6cf9f9e78cc497a8d76fb40e2ccd6
Nabil Birgle <nabil.birgle@gmail.com>
2024-05-04 18:19:27 +0200

    Initial Commit

---
+++ Gesture/GestureApp.swift
@@ -0,0 +1,17 @@
+//
+//  GestureApp.swift
+//  Gesture
+//
+//  Created by Nabil Birgle on 04/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct GestureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
