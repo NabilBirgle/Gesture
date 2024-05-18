08fadf67d4011255e16ca6b0673419a95a1f68eb
Nabil BIRGLE <nabil.birgle@gmail.com>
2024-05-18 11:59:32 +0200

    commit

--- Gesture/ContentView.swift
+++ Gesture/ContentView.swift
@@ -8,15 +8,76 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
+	@State private var isDragging = false
+	var drag: some Gesture {
+		DragGesture()
+			.onChanged(test)
+			.onChanged { _ in self.isDragging = true }
+			.onEnded { d in test(x: d) }
+			.onEnded { _ in self.isDragging = false }
+	}
+	func test(x: DragGesture.Value) -> Void {
+		let d: CGSize = x.translation
+		delta = CGSize(
+			width: Int(d.width*100/circle_width),
+			height: -Int(d.height*100/circle_height)
+		)
+	}
+	@State var circle_width: CGFloat = 0
+	@State var circle_height: CGFloat = 0
+	@State var delta: CGSize = CGSize.init(width: 0, height: 0)
+	let circle = Circle()
+	var body: some View {
+			circle
+				.fill(self.isDragging ? Color.red : Color.blue)
+				.gesture(drag)
+				.getSize{ size in
+					circle_height = size.height
+					circle_width = size.width
+				}
+//				.scaleEffect(magnifyBy)
+				.gesture(magnification)
+		Text("delta: \(delta)")
+		Text("magnify: \(m)")
+		Text("magnifyBy: \(magnifyBy)")
+	}
+
+	@GestureState private var magnifyBy = 1.0
+	@State private var m = 1.0
+	var magnification: some Gesture {
+		MagnifyGesture()
+			.onEnded({value in m = value.magnification})
+			.updating($magnifyBy) { value, gestureState, transaction in
+				gestureState = value.magnification
+			}
+	}
+}
+
+
+
+
+
+
+
+
+
+
+extension View {
+	func getSize(size: @escaping (CGSize) -> Void) -> some View {
+		background(
+			GeometryReader { geometry in
+				Color.clear
+					.preference(key: ViewPreferenceKey.self, value: geometry.size)
+			}
+		)
+		.onPreferenceChange(ViewPreferenceKey.self, perform: size)
+	}
+}
+
+private struct ViewPreferenceKey: PreferenceKey {
+	static var defaultValue: CGSize = .zero
+	static func reduce(value: inout CGSize, nextValue: () -> CGSize) {
+	}
 }
 
 #Preview {
